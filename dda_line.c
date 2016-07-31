c33b10715eed1190385c903a38a42207011a2957
Jayadeep KM <kmjayadeep@gmail.com>
2016-07-31 21:38:29 +0530

    dda line

--- dda_line.c
+++ dda_line.c
@@ -2,46 +2,44 @@
 #include <stdio.h>
 #include <math.h>
 
-int main( )
-{
-    float x,y,x1,y1,x2,y2,dx,dy,pixel;
-    int i,gd,gm;
+int main(){
+	float x,y,x1,y1,x2,y2,dx,dy,pixel;
+	int i,gd,gm;
 
-    printf("Enter the value of x1 : ");
-    scanf("%f",&x1);
-    printf("Enter the value of y1 : ");
-    scanf("%f",&y1);
-    printf("Enter the value of x2 : ");
-    scanf("%f",&x2);
-    printf("Enter the value of y1 : ");
-    scanf("%f",&y2);
+	printf("Enter the value of x1 : ");
+	scanf("%f",&x1);
+	printf("Enter the value of y1 : ");
+	scanf("%f",&y1);
+	printf("Enter the value of x2 : ");
+	scanf("%f",&x2);
+	printf("Enter the value of y1 : ");
+	scanf("%f",&y2);
 
-    detectgraph(&gd,&gm);
-    initgraph(&gd,&gm,"");
+	detectgraph(&gd,&gm);
+	initgraph(&gd,&gm,"");
 
-    dx=abs(x2-x1);
-    dy=abs(y2-y1);
+	dx=abs(x2-x1);
+	dy=abs(y2-y1);
 
-    if(dx>=dy)
-    pixel=dx;
-    else
-    pixel=dy;
+	if(dx>=dy)
+		pixel=dx;
+	else
+		pixel=dy;
 
-    dx=dx/pixel;
-    dy=dy/pixel;
+	dx=dx/pixel;
+	dy=dy/pixel;
 
-    x=x1;
-    y=y1;
+	x=x1;
+	y=y1;
 
-    i=1;
-    while(i<=pixel)
-    {
-          putpixel(x,y,1);
-          x=x+dx;
-          y=y+dy;
-          i=i+1;
-          delay(100);
-    }
-    getch();
-    closegraph();
+	i=1;
+	while(i<=pixel){
+		  putpixel(x,y,1);
+		  x=x+dx;
+		  y=y+dy;
+		  i=i+1;
+		  delay(20);
+	}
+	getch();
+	closegraph();
 }
